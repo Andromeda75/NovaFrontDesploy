1d8e54b8ba0377c9d25b82e07002adfa9a67dd92
Andromeda75 <23090610@alumno.utmetropolitana.edu.mx>
2026-06-30 23:22:06 -0600

    "reinicio"

--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -8,7 +8,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
              <Link to="/categoriaAD" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-1-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="/img/assets/img/iconos/Tab.png" alt="Digital" style={{ height: '35px', objectFit: 'contain' }} />
+              <img src="../src/assets/img/iconos/Tab.png" alt="Digital" style={{ height: '35px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-5 d-none d-md-inline">Arte Digital</span>
             </div>
             </Link>
@@ -17,7 +17,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2 ">
             <Link to="/categoriaAV" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-2-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="/img/assets/img/iconos/Art.png" alt="Visual" style={{ height: '35px', objectFit: 'contain' }} />
+              <img src="../src/assets/img/iconos/Art.png" alt="Visual" style={{ height: '35px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-5 d-none d-md-inline">Arte Visual</span>
             </div>
             </Link>
@@ -25,7 +25,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/categoriaF" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-3-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="/img/assets/img/iconos/Cam.png" alt="Fotografía" style={{ height: '35px', objectFit: 'contain' }} />
+              <img src="../src/assets/img/iconos/Cam.png" alt="Fotografía" style={{ height: '35px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-5 d-none d-md-inline">Fotografía</span>
             </div>
             </Link>
@@ -34,7 +34,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/categoriaE" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-4-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="/img/assets/img/iconos/Head.png" alt="Escultura" style={{ height: '35px', objectFit: 'contain' }} />
+              <img src="../src/assets/img/iconos/Head.png" alt="Escultura" style={{ height: '35px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-5 d-none d-md-inline">Escultura</span>
             </div>
             </Link>
@@ -42,7 +42,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/categoriaA" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-5-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="/img/assets/img/iconos/Han.png" alt="Artesanía" style={{ height: '35px', objectFit: 'contain' }} />
+              <img src="../src/assets/img/iconos/Han.png" alt="Artesanía" style={{ height: '35px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-5 d-none d-md-inline">Artesanía</span>
             </div>
             </Link>
@@ -51,7 +51,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/categoriaC" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-6-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="/img/assets/img/iconos/Cua.png" alt="Coleccionables" style={{ height: '35px', objectFit: 'contain' }} />
+              <img src="../src/assets/img/iconos/Cua.png" alt="Coleccionables" style={{ height: '35px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-5 d-none d-md-inline">Coleccionables</span>
             </div>
             </Link>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import background from "../assets/css/ocean.module.css";
+import background from "../../assets/css/ocean.module.css";
 import logo from "../../assets/img/logos/Logo.png";
 import banner from "../../assets/img/logos/NovaCreattions.png";
 import { authService } from '../../services/authService';
