5100f64adcc8c7cef1248f2d18f47bbeed265283
Andromeda75 <23090610@alumno.utmetropolitana.edu.mx>
2026-06-30 23:57:13 -0600

    "122312"

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+// ✅ SOLO UNA VEZ
+import "/src/assets/css/styles.css";
 import 'datatables.net-dt/css/dataTables.dataTables.css';
 
 import App from './App';
