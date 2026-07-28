1b593afc402d6d9eea3668a5164e8ff696fe785f
Andromeda75 <23090610@alumno.utmetropolitana.edu.mx>
2026-07-28 17:48:52 -0600

    coreccion de imagenes prueba 2

--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -8,7 +8,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
              <Link to="/ArteDigital" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-1-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="../src/assets/img/iconos/Tab.png" alt="Digital" style={{ height: '25px', objectFit: 'contain' }} />
+              <img src="/img/iconos/Tab.png" alt="Digital" style={{ height: '25px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-6 d-none d-md-inline">Arte Digital</span>
             </div>
             </Link>
@@ -17,7 +17,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2 ">
             <Link to="/ArteVisual" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-2-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="../src/assets/img/iconos/Art.png" alt="Visual" style={{ height: '25px', objectFit: 'contain' }} />
+              <img src="/img/iconos/Art.png" alt="Visual" style={{ height: '25px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-6 d-none d-md-inline">Arte Visual</span>
             </div>
             </Link>
@@ -25,7 +25,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/Fotografia" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-3-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="../src/assets/img/iconos/Cam.png" alt="Fotografía" style={{ height: '25px', objectFit: 'contain' }} />
+              <img src="/img/iconos/Cam.png" alt="Fotografía" style={{ height: '25px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-6 d-none d-md-inline">Fotografía</span>
             </div>
             </Link>
@@ -34,7 +34,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/Escultura" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-4-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="../src/assets/img/iconos/Head.png" alt="Escultura" style={{ height: '25px', objectFit: 'contain' }} />
+              <img src="/img/iconos/Head.png" alt="Escultura" style={{ height: '25px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-6 d-none d-md-inline">Escultura</span>
             </div>
             </Link>
@@ -42,7 +42,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/Artesania" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-5-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="../src/assets/img/iconos/Han.png" alt="Artesanía" style={{ height: '25px', objectFit: 'contain' }} />
+              <img src="/img/iconos/Han.png" alt="Artesanía" style={{ height: '25px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-6 d-none d-md-inline">Artesanía</span>
             </div>
             </Link>
@@ -51,7 +51,7 @@ function Navbarc() {
           <div className="col-6 col-md-4 col-lg-4 col-xl-4 col-xxl-2">
             <Link to="/Coleccionables" className="text-decoration-none">
             <div className="card border-0 shadow-sm p-3 border-6-P d-flex flex-row align-items-center justify-content-center gap-1" style={{ borderBottom: '4px solid', borderRadius: '15px' }}>
-              <img src="../src/assets/img/iconos/Cua.png" alt="Coleccionables" style={{ height: '25px', objectFit: 'contain' }} />
+              <img src="/img/iconos/Cua.png" alt="Coleccionables" style={{ height: '25px', objectFit: 'contain' }} />
               <span className="fw-bold color-2 fs-6 d-none d-md-inline">Coleccionables</span>
             </div>
             </Link>
